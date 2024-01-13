8041be36296d88eb482443aeb5171884f0311d70
andresarias7 <andresarias7@hotmail.com>
2024-01-12 23:43:46 -0500

    model

---
+++ base/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def lista_pendientes(pedido):
+    return HttpResponse("lista de pendientes")
+
+# Create your views here.
