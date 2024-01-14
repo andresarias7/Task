79c2c209cfe5222132f990abce1e827404c7fe67
andresarias7 <andresarias7@hotmail.com>
2024-01-13 21:30:55 -0500

    views links and forms

--- base/urls.py
+++ base/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
-from . import views
+from .views import ListaPendientes,DetalleTarea,CrearTarea
 
-urlpatterns = [path("", views.lista_pendientes, name="pendientes")]
+urlpatterns = [path("", ListaPendientes.as_view(), name="tareas"),
+               path("tarea/<int:pk>", DetalleTarea.as_view(), name="tarea"),
+               path("crear-tarea/", CrearTarea.as_view(), name="crear-tarea")]
