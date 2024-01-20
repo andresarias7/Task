a782dc0e66cec49afea058f6e7afdc90edfd286b
andresarias7 <andresarias7@hotmail.com>
2024-01-19 23:17:50 -0500

    Access and especific user

--- base/views.py
+++ base/views.py
@@ -1,8 +1,13 @@
+from typing import Any
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.urls import reverse_lazy
 from .models import Tarea
 
@@ -15,27 +20,37 @@ class Logueo(LoginView):
         return reverse_lazy("tareas")
     
 
-class ListaPendientes(ListView):
+class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
-    context_object_name = "tareas"
+    context_object_name = "tareas" 
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        context["tareas"] = context["tareas"].filter(usuario=self.request.user)
+        context["count"] = context["tareas"].filter(completo = False).count()
+        return context
     
     
-class DetalleTarea(DetailView):
+class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
     context_object_name = "tarea"
     template_name = "base/tarea.html"
     
-class CrearTarea(CreateView):
+class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
-    fields = "__all__"
+    fields = ["titulo", "descripcion", "completo"]
     success_url = reverse_lazy("tareas")
     
-class EditarTarea(UpdateView):
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
+    
+class EditarTarea(LoginRequiredMixin, UpdateView):
      model = Tarea
-     fields = "__all__"
+     fields = ["titulo", "descripcion", "completo"]
      success_url = reverse_lazy("tareas")
      
-class EliminarTarea(DeleteView):
+class EliminarTarea(LoginRequiredMixin, DeleteView):
      model = Tarea
      context_object_name = "tareas"
      success_url = reverse_lazy("tareas")
